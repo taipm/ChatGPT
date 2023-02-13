fa55131e6bf668eb8630927ace139721dc17de5b
Phan Minh Tài <taipm.vn@gmail.com>
2023-02-13 11:25:27 +0700

    update for first

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from ssl import CertificateError
 from flask import Flask
 
 # app = Flask(__name__)
@@ -15,13 +16,18 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 #dBjKFnQLiLHM1Jyd
-app.secret_key = "secret_key"
+app.secret_key = "dBjKFnQLiLHM1Jyd"
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["blog_db"]
+#client = MongoClient("mongodb://localhost:27017/")
+#db = client["BlogGPT"]
+#users_collection = db["users"]
+
+
+client = MongoClient("mongodb+srv://taipm:dBjKFnQLiLHM1Jyd@cluster0.op5mel7.mongodb.net/?retryWrites=true&w=majority")
+db = client['BlogGPT']
 users_collection = db["users"]
 
 class User(UserMixin):
@@ -42,6 +48,26 @@ def load_user(user_id):
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        email = request.form.get('email')
+        if username and password and email:
+            existing_user = User.objects(username=username).first()
+            if existing_user is None:
+                hash_password = generate_password_hash(password)
+                user = User(username=username, password=hash_password, email=email).save()
+                flash('Đăng ký thành công! Hãy đăng nhập.')
+                return redirect(url_for('login'))
+            else:
+                flash('Tên người dùng đã tồn tại! Vui lòng chọn tên khác.')
+        else:
+            flash('Vui lòng điền đầy đủ thông tin!')
+    return render_template('register.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
